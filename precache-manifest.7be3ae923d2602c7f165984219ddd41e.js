4d0835c92ef2c51b2b4eb1abf1c1846d9dd51415
HanifullahJamalzai <43265047+HanifullahJamalzai@users.noreply.github.com>
2020-06-18 14:09:03 +0430

    Updates

---
+++ precache-manifest.7be3ae923d2602c7f165984219ddd41e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "caf6792ef776643e0496f683bd017553",
+    "url": "/cat_rolodox/index.html"
+  },
+  {
+    "revision": "c074005dc5cf80ed3c3f",
+    "url": "/cat_rolodox/static/css/2.e2dd9b0a.chunk.css"
+  },
+  {
+    "revision": "8855a6460cec035ac210",
+    "url": "/cat_rolodox/static/css/main.ccf54095.chunk.css"
+  },
+  {
+    "revision": "c074005dc5cf80ed3c3f",
+    "url": "/cat_rolodox/static/js/2.58171e3b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cat_rolodox/static/js/2.58171e3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8855a6460cec035ac210",
+    "url": "/cat_rolodox/static/js/main.41e0b6ab.chunk.js"
+  },
+  {
+    "revision": "97e7d2c83d0eb06c19e4",
+    "url": "/cat_rolodox/static/js/runtime-main.5ecbf5af.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/cat_rolodox/precache-manifest.7be3ae923d2602c7f165984219ddd41e.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/cat_rolodox/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
